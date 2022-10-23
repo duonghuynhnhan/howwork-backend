4727b027470b924d06a34600307251d8b9cb68f4
duonghuynhnhan <dhuynhnhan@gmail.com>
2022-10-23 20:36:07 +0700

    Running server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ messsage: 'Welcome to howwork-backend'});
+})
+
+module.exports = app;
