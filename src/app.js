558be578f6bbde35c43b380ac8bf6e5e870ac76d
duonghuynhnhan <dhuynhnhan@gmail.com>
2022-11-19 19:11:52 +0700

    API

--- src/app.js
+++ src/app.js
@@ -1,28 +1,89 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const howworkController = require("./controllers/howwork.controller");
-const ApiError = require("./api-error");
+const howworkController = require('./controllers/howwork.controller')
+const ApiError = require('./api-error')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.json({ messsage: "Welcome to howwork-backend" });
-});
+app.get('/', (req, res) => {
+    res.json({ messsage: 'Welcome to howwork-backend' })
+})
 
-app.route("/api/home").get(howworkController.home);
+// ACCOUNT
+app.route('/api/account/login').post(howworkController.login)
+app.route('/api/account/forgot-password').post(howworkController.forgotPassword)
+
+// USER
+// userHome
+app.route('/api/user/home').get(howworkController.userHome)
+
+// userProjects
+app.route('/api/user/projects').get(howworkController.userProjects)
+app.route('/api/user/project-detail').get(howworkController.userProjectDetail)
+
+// userTasks
+app.route('/api/user/tasks').get(howworkController.userTasks)
+app.route('/api/user/task-detail').get(howworkController.userTaskDetail)
+
+// userSummary
+app.route('/api/user/summary').get(howworkController.userSummary)
+
+// userInfo
+app.route('/api/user/information').get(howworkController.userInformation)
+app.route('/api/user/change-information/:id').put(howworkController.userChangeInformation)
+app.route('/api/user/change-password/:id').put(howworkController.userChangePassword)
+app.route('/api/user/change-key/:id').put(howworkController.userChangeKey)
+
+// userNotifications
+app.route('/api/user/notifications').get(howworkController.userNotifications)
+
+// ADMIN
+// adminHome
+app.route('/api/admin/home').get(howworkController.adminHome)
+
+// adminProjects
+app.route('/api/admin/create-project').post(howworkController.adminCreateProject)
+app.route('/api/admin/projects').get(howworkController.adminProjects)
+app.route('/api/admin/project-detail').get(howworkController.adminProjectDetail)
+app.route('/api/admin/edit-project/:id').put(howworkController.adminEditProject)
+app.route('/api/admin/delete-project/:id').delete(howworkController.adminDeleteProject)
+
+// adminTasks
+app.route('/api/admin/create-task').post(howworkController.adminCreateTask)
+app.route('/api/admin/tasks').get(howworkController.adminTasks)
+app.route('/api/admin/task-detail').get(howworkController.adminTaskDetail)
+app.route('/api/admin/edit-task/:id').put(howworkController.adminEditTask)
+app.route('/api/admin/delete-task/:id').delete(howworkController.adminDeleteTask)
+
+// adminAccounts
+app.route('/api/admin/create-user').post(howworkController.adminCreateUser)
+app.route('/api/admin/delete-user/:id').delete(howworkController.adminDeleteUser)
+app.route('/api/admin/create-admin').post(howworkController.adminCreateAdmin)
+
+// adminSummary
+app.route('/api/admin/summary').get(howworkController.adminSummary)
+
+// adminInfo
+app.route('/api/admin/information').get(howworkController.adminInformation)
+app.route('/api/admin/change-information/:id').put(howworkController.adminChangeInformation)
+app.route('/api/admin/change-password/:id').put(howworkController.adminChangePassword)
+app.route('/api/admin/change-key/:id').put(howworkController.adminChangeKey)
+
+// adminNotifications
+app.route('/api/admin/notifications').get(howworkController.adminNotifications)
 
 app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-});
+    return next(new ApiError(404, 'Resource not found'))
+})
 
 app.use((error, req, res, next) => {
     return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
+        message: error.message || 'Internal Server Error',
+    })
+})
 
-module.exports = app;
+module.exports = app
--- src/controllers/howwork.controller.js
+++ src/controllers/howwork.controller.js
@@ -1,3 +1,134 @@
-exports.home = (req, res) => {
-    return res.send({ message: 'Home' });
-};
+// ACCOUNT
+exports.login = (req, res) => {
+    return res.send({ message: 'Login' })
+}
+
+exports.forgotPassword = (req, res) => {
+    return res.send({ message: 'forgotPassword' })
+}
+
+// USER
+exports.userHome = (req, res) => {
+    return res.send({ message: 'userHome' })
+}
+
+exports.userProjects = (req, res) => {
+    return res.send({ message: 'userProjects' })
+}
+
+exports.userProjectDetail = (req, res) => {
+    return res.send({ message: 'userProjectDetail' })
+}
+
+exports.userTasks = (req, res) => {
+    return res.send({ message: 'userTaks' })
+}
+
+exports.userTaskDetail = (req, res) => {
+    return res.send({ message: 'userTaskDetail' })
+}
+
+exports.userSummary = (req, res) => {
+    return res.send({ message: 'userSummary' })
+}
+
+exports.userInformation = (req, res) => {
+    return res.send({ message: 'userInformation' })
+}
+
+exports.userChangeInformation = (req, res) => {
+    return res.send({ message: 'userChangeInformation' })
+}
+
+exports.userChangePassword = (req, res) => {
+    return res.send({ message: 'userChangePassword' })
+}
+
+exports.userChangeKey = (req, res) => {
+    return res.send({ message: 'userChangeKey' })
+}
+
+exports.userNotifications = (req, res) => {
+    return res.send({ message: 'userNotifications' })
+}
+
+// ADMIN
+exports.adminHome = (req, res) => {
+    return res.send({ message: 'adminHome' })
+}
+
+exports.adminCreateProject = (req, res) => {
+    return res.send({ message: 'adminCreateProject' })
+}
+
+exports.adminProjects = (req, res) => {
+    return res.send({ message: 'adminProjects' })
+}
+
+exports.adminProjectDetail = (req, res) => {
+    return res.send({ message: 'adminProjectDetail' })
+}
+
+exports.adminEditProject = (req, res) => {
+    return res.send({ message: 'adminEditProject' })
+}
+
+exports.adminDeleteProject = (req, res) => {
+    return res.send({ message: 'adminDeleteProject' })
+}
+
+exports.adminCreateTask = (req, res) => {
+    return res.send({ message: 'adminCreateTask' })
+}
+
+exports.adminTasks = (req, res) => {
+    return res.send({ message: 'adminTasks' })
+}
+
+exports.adminTaskDetail = (req, res) => {
+    return res.send({ message: 'adminTaskDetail' })
+}
+
+exports.adminEditTask = (req, res) => {
+    return res.send({ message: 'adminEditTask' })
+}
+
+exports.adminDeleteTask = (req, res) => {
+    return res.send({ message: 'adminDeleteTask' })
+}
+
+exports.adminCreateUser = (req, res) => {
+    return res.send({ message: 'adminCreateUser' })
+}
+
+exports.adminDeleteUser = (req, res) => {
+    return res.send({ message: 'adminDeleteUser' })
+}
+
+exports.adminCreateAdmin = (req, res) => {
+    return res.send({ message: 'adminCreateAdmin' })
+}
+
+exports.adminSummary = (req, res) => {
+    return res.send({ message: 'adminSummary' })
+}
+
+exports.adminInformation = (req, res) => {
+    return res.send({ message: 'adminInformation' })
+}
+
+exports.adminChangeInformation = (req, res) => {
+    return res.send({ message: 'adminChangeInformation' })
+}
+
+exports.adminChangePassword = (req, res) => {
+    return res.send({ message: 'adminChangePassword' })
+}
+
+exports.adminChangeKey = (req, res) => {
+    return res.send({ message: 'adminChangeKey' })
+}
+
+exports.adminNotifications = (req, res) => {
+    return res.send({ message: 'adminNotifications' })
+}
